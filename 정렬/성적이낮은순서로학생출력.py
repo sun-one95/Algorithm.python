c94b8c14408464e80c4ec82c8c0570e28fd7ad6f
sun-one95 <isay0693@gmail.com>
2021-10-15 11:37:52 +0900

    입력값 받는 코드 수정

--- 정렬/성적이낮은순서로학생출력.py
+++ 정렬/성적이낮은순서로학생출력.py
@@ -1,13 +1,18 @@
 n = int(input())
 arr = []
 for i in range(n):
-    arr.append(input().split())
+    input_data = input().split()
+    # 이름은 문자열 그대로, 점수는 정수형으로 변환하여 저장
+    arr.append((input_data[0], int(input_data[1])))
 
-# 성적순으로 정렬하기 위해 함수 설정
-def setting(data):
-    return data[1]
+# 내 풀이 
+# # 성적순으로 정렬하기 위해 함수 설정
+# def setting(data):
+#     return data[1]
+# result = sorted(arr, key= setting) # 성적순으로 배열 정렬
 
-result = sorted(arr, key= setting) # 성적순으로 배열 정렬
+# 참조 풀이
+result = sorted(arr, key=lambda student: student[1])
 
 # 이름만 출력하기 위해 출력문 작성
 for i in result:
