2660b8557014c3603f783f661054a74796226f6c
sun-one95 <isay0693@gmail.com>
2021-10-26 13:16:26 +0900

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,23 +1,21 @@
 import sys
 
-n, m = list(map(int, sys.stdin.readline().split()))
+n = int(sys.stdin.readline())
 arr = list(map(int, sys.stdin.readline().split()))
 
-start = 0
-end = max(arr)
+def bs(arr, start, end):
+    while start <= end:
+        mid = (start + end) // 2
+        if arr[mid] == mid:
+            return mid
+        elif arr[mid] > mid:
+            end = mid - 1
+        else:
+            start = mid + 1
+    return None
 
-high = 0
-while (start <= end):
-    total = 0
-    mid = (start + end) // 2
-    for x in arr:
-        if x > mid:
-            total += x - mid
-
-    if total < m:
-        end = mid - 1
-    else:
-        high = mid
-        start = mid + 1
-
-print(high)
+index = bs(arr, 0, n - 1)
+if index == None:
+    print(-1)
+else:
+    print(index)
