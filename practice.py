cbe6e382385bcb46a2bd8e5b060e2dd4068ac552
sun-one95 <isay0693@gmail.com>
2021-11-25 22:56:43 +0900

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,35 +1,34 @@
 from collections import deque
 
 n, k = map(int, input().split())
-graph = [] # 리스트 정보를 담은 배열
-data = [] # 바이러스 정보를 담은 배열
+graph = [] # 전체적인 맵의 정보를 담은 리스트
+data = [] # 바이러스 정보를 담은 리스트
 
 for i in range(n):
     graph.append(list(map(int, input().split())))
     for j in range(n):
-        # 바이러스, 시간, x 위치, y 위치
-        data.append((graph[i][j], 0, i, i))
+        if graph[i][j] != 0: # 바이러스일 경우
+            # 바이러스 종류, 시간, x, y 위치를 삽입
+            data.append((graph[i][j], 0, i, j))
 
-data.sort()
+data.sort() # 낮은 순서대로 바이러스 전염을 해야 하므로
+q = deque(data)
 
 target_s, target_x, target_y = map(int, input().split())
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, 1, -1]
 
-q = deque(data)
-
 while q:
     virus, s, x, y = q.popleft()
-    if s == target_s:
+    if s == target_s: # 정해진 시간과 같으면 종료
         break
     for i in range(4):
         nx = x + dx[i]
         ny = y + dy[i]
         if 0 <= nx and nx < n and 0 <= ny and ny < n:
-            # 방문하지 않았다면 그 위치에 바이러스 넣기
-            if graph[nx][ny] == 0:
-                graph[nx][ny] = virus
+            if graph[nx][ny] == 0: # 아직 방문하지 않았다면
+                graph[nx][ny] = virus # 전염시작
                 q.append((virus, s + 1, nx, ny))
 
 print(graph[target_x - 1][target_y - 1])
