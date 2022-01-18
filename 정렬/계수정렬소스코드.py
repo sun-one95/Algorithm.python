8d4c852e42a85c02e0ba1572d03594c29bedad1f
sun-one95 <isay0693@gmail.com>
2022-01-18 23:42:46 +0900

    Update 계수정렬소스코드.py

--- 정렬/계수정렬소스코드.py
+++ 정렬/계수정렬소스코드.py
@@ -10,9 +10,11 @@ arr = [7, 5, 9, 0, 3, 1, 6, 2, 9, 1, 4, 8, 0, 5, 2]
 # 모든 범위를 포함하는 리스트 선언(모든 값은 0으로 초기화)
 count = [0] * (max(arr) + 1)
 
-for i in range(len(arr)):
-    count[arr[i]] += 1 # 각 데이터에 해당하는 인덱스의 값 증가
+# for i in range(len(arr)):
+#     count[arr[i]] += 1 # 각 데이터에 해당하는 인덱스의 값 증가
 
-for i in range(len(count)): # 리스트에 기록된 정보 확인
-    for j in range(count[i]):
-        print(i, end=' ') # 띄어쓰기를 구분으로 등장한 횟수만큼 인덱스 출력
+# for i in range(len(count)): # 리스트에 기록된 정보 확인
+#     for j in range(count[i]):
+#         print(i, end=' ') # 띄어쓰기를 구분으로 등장한 횟수만큼 인덱스 출력
+
+print(count)
