c5df08fcd8bcffdf83614a7a27bbab20a1c446af
sun-one95 <isay0693@gmail.com>
2022-01-28 23:34:45 +0900

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,69 +1,65 @@
 from collections import deque
-INF = 1e9
 
-n = int(input())
-arr = []
-for i in range(n):
-    arr.append(list(map(int, input().split())))
+for tc in range(int(input())):
+    n = int(input())
 
-now_size = 2
-now_x, now_y = 0, 0
+    graph = [[False] * (n + 1) for _ in range(n + 1)]
+    indegree = [0] * (n + 1)
 
-for i in range(n):
-    for j in range(n):
-        if arr[i][j] == 9:
-            now_x, now_y = i, j
-            arr[now_x][now_y] = 0
+    data = list(map(int, input().split()))
 
-dx = [-1, 0, 1, 0]
-dy = [0, 1, 0, -1]
-
-def bfs():
-    dist = [[-1] * n for i in range(n)]
-    q = deque([(now_x, now_y)])
-    dist[now_x][now_y] = 0
-    while q:
-        x, y = q.popleft()
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if 0 <= nx and nx < n and 0 <= ny and ny < n:
-                if dist[nx][ny] == -1 and now_size >= arr[nx][ny]:
-                    dist[nx][ny] = dist[x][y] + 1
-                    q.append((nx, ny))
-
-    return dist
-
-def find(dist):
-    x, y = 0, 0
-    min_dist = INF
     for i in range(n):
-        for j in range(n):
-            if dist[i][j] != -1 and 1 < arr[i][j] and arr[i][j] < now_size:
-                if dist[i][j] < min_dist:
-                    min_dist = dist[i][j]
-                    x, y = i, j
+        for j in range(i + 1, n):
+            graph[data[i]][data[j]] = True
+            indegree[data[j]] += 1
+
+    m = int(input())
+    for i in range(m):
+        a, b = map(int, input().split())
+        if graph[a][b]:
+            graph[a][b] = False
+            graph[b][a] = True
+            indegree[a] += 1
+            indegree[b] -= 1
+        else:
+            graph[a][b] = True
+            graph[b][a] = False
+            indegree[a] -= 1
+            indegree[b] += 1
+
+    cycle = False
+    certain = True
+
+    result = []
+    q = deque()
+    for i in range(1, n + 1):
+        if indegree[i] == 0:
+            q.append(i)
+
+    for i in range(n):
+        if len(q) == 0:
+            cycle = True
+            break
+        if len(q) >= 2:
+            certain = False
+            break
+        now = q.popleft()
+        result.append(now)
+        for j in range(1, n + 1):
+            if graph[now][j]:
+                indegree[j] -= 1
+                if indegree[j] == 0:
+                    q.append(j)
+
+    if cycle:
+        print('IMPOSSIBLE')
+    elif not certain:
+        print('?')
+    else:
+        for i in result:
+            print(i, end=' ')
+        print()
+
     
-    if min_dist == INF:
-        return None
-    else:
-        return x, y, min_dist
-
-result = 0
-ate = 0
-
-while True:
-    value = find(bfs())
-    if value == None:
-        print(result)
-        break
-    else:
-        now_x, now_y = value[0], value[1]
-        result += value[2]
-        arr[now_x][now_y] = 0
-        ate += 1
-        if ate >= now_size:
-            now_size += 1
-            ate = 0
 
     
