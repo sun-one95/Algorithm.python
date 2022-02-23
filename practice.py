649dfd2c9c530b1c12770e0dd89b30e01d9684dd
sun-one95 <isay0693@gmail.com>
2022-02-23 23:08:08 +0900

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,85 +1,57 @@
-from __future__ import barry_as_FLUFL
-from itertools import combinations
-from optparse import BadOptionError
-from os import spawnlp
+n = int(input()) # 보드의 크기
+k = int(input()) # 사과의 개수
+board = [[0] * (n + 1) for _ in range(n + 1)] # 보드
+info = [] # 방향 정보 리스트
 
-n = int(input())
-board = [] # 복도 정보
-teachers = [] # 모든 선생님 위치 정보
-spaces = [] # 모든 빈 공간 위치 정보
+for _ in range(k):
+    a, b = map(int, input().split())
+    board[a][b] = 1
 
-for i in range(n):
-    board.append(list(input().split()))
-    for j in range(n):
-        if board[i][j] == 'T':
-            teachers.append((i, j))
-        # 장애물을 설치할 수 있는(빈 공간) 위치 저장
-        if board[i][j] == 'X':
-            spaces.append((i, j))
+l = int(input())
+for _ in range(l):
+    x, c = input().split()
+    info.append((int(x), c))
 
-# 특정 방향으로 감시를 진행(학생 발견: True, 학생 미발견: False)
-def watch(x, y, direction):
-    # 왼쪽 방향으로 감시
-    if direction == 0:
-        while y >= 0:
-            if board[x][y] == 'S': # 학생이 있는 경우
-                return True
-            if board[x][y] == 'O': # 장애물이 있는 경우
-                return False
-            y -= 1
-    # 오른쪽 방향으로 감식
-    if direction == 1:
-        while y < n:
-            if board[x][y] == 'S': # 학생이 있는 경우
-                return True
-            if board[x][y] == 'O': # 장애물이 있는 경우
-                return False
-            y += 1
-    # 위쪽 방향으로 감시
-    if direction == 2:
-        while x >= 0:
-            if board[x][y] == 'S': # 학생이 있는 경우
-                return True
-            if board[x][y] == 'O': # 장애물이 있는 경우
-                return False
-            x -= 1
-    # 아래쪽 방향으로 감시
-    if direction == 1:
-        while x < n:
-            if board[x][y] == 'S': # 학생이 있는 경우
-                return True
-            if board[x][y] == 'O': # 장애물이 있는 경우
-                return False
-            x += 1
-    return False
+def turn(direction, c):
+    if c == 'L':
+        direction = (direction - 1) % 4
+    else:
+        direction = (direction + 1) % 4
+    return direction
 
-# 장애물 설치 이후에, 한 명이라도 학생이 감지되는지 검사
-def process():
-    # 모든 선생님의 위치를 하나씩 확인
-    for x, y in teachers:
-        # 4가지 방향으로 학생을 감지할 수 있는지 확인
-        for i in range(4):
-            if watch(x, y, i):
-                return True
-    return False
+# 동, 남, 서, 북 순으로 이동
+dx = [0, 1, 0, -1]
+dy = [1, 0, -1, 0]
 
-find = False # 학생이 한 명도 감지되지 않도록 설치할 수 있는지의 여부
+def simulate():
+    x, y = 0, 0 # 출발점
+    direction = 0 # 동쪽으로 출발
+    board[x][y] = 2
+    q = [(x, y)]
+    time = 0
+    index = 0
+    while True:
+        nx = x + dx[direction]
+        ny = y + dy[direction]
+        if 0 <= nx and nx < n and 0 <= ny and ny < n and board[nx][ny] != 2:
+            if board[nx][ny] == 0:
+                px, py = q.pop(0)
+                board[px][py] = 0
+                board[nx][ny] = 2
+                q.append((nx, ny))
+            if board[nx][ny] == 1:
+                board[nx][ny] = 2
+                q.append((nx, ny))
+        else:
+            time += 1
+            break
+        time += 1
+        x, y = nx, ny
+        if index < l and time == info[index][0]:
+            direction = turn(direction, info[index][1])
+            index += 1
+        
 
-# 빈 공간에서 3개를 뽑는 모든 조합을 확인
-for data in combinations(spaces, 3):
-    # 장애물 설치해보기
-    for x, y in data:
-        board[x][y] = 'O'
-    # 학생이 한 명도 감지되지 않는 경우
-    if not process():
-        # 원하는 경우를 발견한 것임
-        find = True
-        break
-    # 설치된 장애물을 다시 없애기
-    for x, y in data:
-        board[x][y] = 'X'
+    return time
 
-if find:
-    print('YES')
-else:
-    print('NO')
+print(simulate())
