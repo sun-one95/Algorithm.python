4ddc8849fb83c486fb79847f6a97d9eb756fbd5e
sun-one95 <isay0693@gmail.com>
2023-02-06 00:47:48 +0900

    Create 소인수분해.py

---
+++ 소수판별/소인수분해.py
@@ -0,0 +1,16 @@
+'''
+백준 11653
+'''
+n = int(input())
+
+if n == 1:
+    print('')
+
+# 2부터 하나씩 나눠보기
+for i in range(2, n + 1):
+    if n % i == 0:
+        # 해당 숫자를 나눌 수 없을 때까지 나누기
+        while n % i == 0:
+            print(i)
+            n = n / i
+
