08d1b33e453e2f64e7fea068a7bb11e786f4f3ef
sun-one95 <isay0693@gmail.com>
2023-03-07 23:50:16 +0900

    Update 계단오르기.py

--- 다이나믹프로그래밍/계단오르기.py
+++ 다이나믹프로그래밍/계단오르기.py
@@ -50,19 +50,19 @@ dp = [
 '''
 
 import sys
+
 input = sys.stdin.readline
-arr = []
 
+n = int(input())
+arr = [0 for i in range(301)]
+for i in range(n):
+    arr[i] = int(input())
 
-l = int(input())
-dp = [0] * l
-for _ in range(l):
-    arr.append(int(input()))
-
+dp = [0 for i in range(301)]
 dp[0] = arr[0]
 dp[1] = arr[0] + arr[1]
-dp[2] = max(arr[0] + arr[2], arr[1] + arr[2])
-for i in range(3,l):
-    dp[i] = max(dp[i-2] + arr[i] , dp[i-3] + arr[i] + arr[i - 1])
+dp[2] = max(arr[0] + arr[2], arr[1] + arr[2]) 
+for i in range(3, n):
+    dp[i] = max(dp[i - 3] + arr[i-1] + arr[i], dp[i - 2] + arr[i])
 
-print(dp)
+print(dp[n - 1])
