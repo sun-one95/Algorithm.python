4158ab55fb97f67a3ac5c34607258e4cf3f7efce
sun-one95 <isay0693@gmail.com>
2023-05-17 23:55:19 +0900

    Update 신입사원.py

--- 그리디/신입사원.py
+++ 그리디/신입사원.py
@@ -1,31 +1,31 @@
 '''
 백준 1946
 
+3 2
 1 4
-2 5
-3 6
-4 2
-5 7
-6 1  [(1 4), (4, 2), (6, 1)]
-7 3
+4 1
+2 3
+5 5
+rank = [(1, 4), (2, 3), (3, 2), (4, 1), (5, 5)]
+
 '''
 
 import sys
+
 input = sys.stdin.readline
 
-t = int(input())
-
-for tc in range(t):
+for tc in range(int(input())):
     n = int(input())
     rank = []
     for i in range(n):
         rank.append(list(map(int, input().split())))
-    rank_asc = rank.sort()
+
+    rank.sort()
     top = 0
     result = 1
 
-    for i in range(1, len(rank_asc)):
-        if rank_asc[i][1] < rank_asc[top][1]:
+    for i in range(1, n):
+        if rank[i][1] < rank[top][1]:
             top = i
             result += 1
 
