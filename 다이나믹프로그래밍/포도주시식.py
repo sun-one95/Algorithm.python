c7ceb98e8c19991df92b5e04a5e2c5f06b4ffa76
sun-one95 <isay0693@gmail.com>
2023-07-18 09:15:51 +0900

    Create 포도주시식.py

---
+++ 다이나믹프로그래밍/포도주시식.py
@@ -0,0 +1,15 @@
+'''
+백준 2156
+'''
+
+n = int(input())
+wines = [0] * 10000
+for i in range(n):
+    wines[i] = int(input())
+
+dp = [0] * 10000
+dp[0] = wines[0]
+dp[1] = wines[0] + wines[1]
+dp[2] = max(wines[0] + wines[1], wines[0] + wines[2])
+for i in range(3, n):
+    dp[i] = max(wines[i] + dp[i-2], wines[i] + wines[i-1] + dp[i-3], dp[i-1])
