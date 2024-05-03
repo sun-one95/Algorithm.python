9c32f5a76680ad39bc0a399d968d0ca88569a2de
sun-one95 <isay0693@gmail.com>
2024-05-03 10:16:22 +0900

    Create ??!.py

---
+++ 구현/??!.py
@@ -0,0 +1,13 @@
+'''
+백준 10926
+
+입력으로 받은 이름과 '??!' 문자열을 합쳐서 출력한다.
+파이썬의 f-string을 이용하여 f'{}' 답을 리턴하였다.
+'''
+
+name = input()
+exist_name = f'{name}??!'
+
+print(exist_name)
+# print(name + "??!")
+
